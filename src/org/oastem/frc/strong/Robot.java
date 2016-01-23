dc41801ce0cdcc7270a50105d44069f3b67cdf33
Joie Le <joie188@gmail.com>
2016-01-23 10:20:56 -0800

    Add drive system and arcade drive

--- src/org/oastem/frc/strong/Robot.java
+++ src/org/oastem/frc/strong/Robot.java
@@ -1,14 +1,10 @@
-
 package org.oastem.frc.strong;
-
-
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.RobotDrive;
+import org.oastem.frc.control.DriveSystem;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 /**
  * This is a demo program showing the use of the RobotDrive class.
  * The SampleRobot class is the base of a robot application that will automatically call your
@@ -26,65 +22,39 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
 public class Robot extends SampleRobot {
-    RobotDrive myRobot;
-    Joystick stick;
+	private final int FRONT_LEFT_DRIVE = 1;
+	private final int FRONT_RIGHT_DRIVE = 3;
+	private final int BACK_LEFT_DRIVE = 0;
+	private final int BACK_RIGHT_DRIVE = 2;
+	private static double joyScale = 1.0;
+    DriveSystem myRobot = DriveSystem.getInstance();
+    Joystick stickLeft;
+    Joystick stickRight;
     final String defaultAuto = "Default";
     final String customAuto = "My Auto";
     SendableChooser chooser;
 
+    
     public Robot() {
-        myRobot = new RobotDrive(0, 1);
-        myRobot.setExpiration(0.1);
-        stick = new Joystick(0);
+    	myRobot.initializeDrive(FRONT_LEFT_DRIVE, BACK_LEFT_DRIVE, FRONT_RIGHT_DRIVE, BACK_RIGHT_DRIVE); //WE ARE SMART
+        stickLeft = new Joystick(0);
+        stickRight = new Joystick(1);
     }
     
     public void robotInit() {
-        chooser = new SendableChooser();
+    	chooser = new SendableChooser();
         chooser.addDefault("Default Auto", defaultAuto);
         chooser.addObject("My Auto", customAuto);
         SmartDashboard.putData("Auto modes", chooser);
-    }
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
-    public void autonomous() {
-    	
-    	String autoSelected = (String) chooser.getSelected();
-//		String autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
-    	
-    	switch(autoSelected) {
-    	case customAuto:
-            myRobot.setSafetyEnabled(false);
-            myRobot.drive(-0.5, 1.0);	// spin at half speed
-            Timer.delay(2.0);		//    for 2 seconds
-            myRobot.drive(0.0, 0.0);	// stop robot
-            break;
-    	case defaultAuto:
-    	default:
-            myRobot.setSafetyEnabled(false);
-            myRobot.drive(-0.5, 0.0);	// drive forwards half speed
-            Timer.delay(2.0);		//    for 2 seconds
-            myRobot.drive(0.0, 0.0);	// stop robot
-            break;
-    	}
     }
 
     /**
      * Runs the motors with arcade steering.
      */
     public void operatorControl() {
-        myRobot.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
-            myRobot.arcadeDrive(stick); // drive with arcade style (use right stick)
-            Timer.delay(0.005);		// wait for a motor update time
+            myRobot.arcadeDrive(stickLeft.getY(), stickLeft.getX()); // drive with arcade style (use right stick)
+            //myRobot.tankDrive(stickLeft.getY(), stickRight.getY());
         }
     }
 
@@ -93,4 +63,46 @@ public class Robot extends SampleRobot {
      */
     public void test() {
     }
+    
+    private void doArcadeDrive(boolean hasAccel) {
+		double leftMove = 0.0;
+		double rightMove = 0.0;
+		double zone = 0.04;
+
+		joyScale = scaleZ(stickLeft.getZ());
+
+		double x = stickLeft.getX();
+		double y = stickLeft.getY() * -1;
+
+		if (Math.abs(y) > zone) 
+		{
+			leftMove = y;
+			rightMove = y;
+		}
+
+		if (Math.abs(x) > zone) 
+		{
+			leftMove = correct(leftMove + x);
+			rightMove = correct(rightMove - x);
+		}
+
+		leftMove *= joyScale * -1;
+		rightMove *= joyScale * -1;
+
+		myRobot.tankDrive(leftMove, rightMove);
+	}
+
+	private double scaleZ(double rawZ) {
+		return Math.min(1.0, 0.5 - 0.5 * rawZ);
+	}
+
+	private double correct(double val) {
+		if (val > 1.0) {
+			return 1.0;
+		}
+		if (val < -1.0) {
+			return -1.0;
+		}
+		return val;
+	}
 }
