f4f780c4b8028415c29d16cb2027ac50aa4d311b
mandarker <springnguyen98@gmail.com>
2016-02-04 17:52:12 -0800

    Configure and work on ImageProcessing further, create enums

--- src/org/oastem/frc/sensor/ImageProcessing.java
+++ src/org/oastem/frc/sensor/ImageProcessing.java
@@ -7,92 +7,242 @@ import java.util.ArrayList;
  * @author mandarker
  */
 
-public class ImageProcessing{
-	
+public class ImageProcessing {
+
 	private NetworkTable table;
-	
-	//This defaultValue is here in the case that there is nothing detected, so that there would be no error.
-	private double[] defaultValue;
-	
-	/**
-	 * Initializes a Network Table that retrieves line values from the GRIP image processor. Make sure
-	 * GRIP is connected to the robot and "Publish LinesReport" is a connected operation in the
-	 * operation flowchart.
-	 * @param address The address for the Network Table that can be found in the Outline Viewer.
-	 */
-	public ImageProcessing(String address){
-		table = NetworkTable.getTable(address);
-        defaultValue = new double[0];
+
+	// This defaultValue is here in the case that there is nothing detected on
+	// the Network Table,
+	// so that there would be no error.
+
+	public enum ProcessingType {
+		Lines, Contours, Blobs;
 	}
-	
+
+	private ProcessingType process;
+
+	private String contours;
+	private String lines;
+	private String blobs;
+	private double[] defaultValue;
+
+	/**
+	 * Initializes a Network Table that retrieves line values from the GRIP
+	 * image processor. Make sure GRIP is connected to the robot and
+	 * "Publish LinesReport" is a connected operation in the operation
+	 * flowchart.
+	 * 
+	 * @param address
+	 *            The address for the Network Table that can be found in the
+	 *            Outline Viewer.
+	 */
+	public ImageProcessing(String contours, String lines, String blobs, ProcessingType type) {
+		this.contours = contours;
+		this.lines = lines;
+		this.blobs = blobs;
+		switchType(type);
+		defaultValue = new double[0];
+	}
+
+	public void switchType(ProcessingType type) {
+		switch (type) {
+		case Lines:
+			table = NetworkTable.getTable(lines);
+			break;
+		case Contours:
+			table = NetworkTable.getTable(contours);
+			break;
+		case Blobs:
+			table = NetworkTable.getTable(blobs);
+			break;
+		}
+		process = type;
+	}
+
 	/**
 	 * Gets the angle of each line detected by the GRIP image processor.
-	 * @return The angle of each line retrieved from the Network Table published by the "Publish LinesReport" operation.
+	 * 
+	 * @return The angle of each line retrieved from the Network Table published
+	 *         by the "Publish LinesReport" operation.
 	 */
-	public double[] getAngles(){
-		return table.getNumberArray("angle", defaultValue);
+	public ArrayList<Double> getAngles() {
+		switchType(ProcessingType.Lines);
+		double[] current = table.getNumberArray("angle", defaultValue);
+
+		return convert(current);
 	}
-	
+
 	/**
 	 * Gets the length of each line detected by the GRIP image processor.
-	 * @return The length of each line retrieved from the Network Table published by the "Publish LinesReport" operation.
+	 * 
+	 * @return The length of each line retrieved from the Network Table
+	 *         published by the "Publish LinesReport" operation.
 	 */
-	public double[] getLengths(){
-		return table.getNumberArray("length", defaultValue);
+	public ArrayList<Double> getLengths() {
+		switchType(ProcessingType.Lines);
+		double[] current = table.getNumberArray("length", defaultValue);
+
+		return convert(current);
 	}
-	
+
 	/**
-	 * Gets the raw values of the two points creating each line, straight from the Network Table.
-	 * @return The raw points of each line retrieved from the Network Table published by the "Publish LinesReport" operation.
+	 * Gets the raw values of the two points creating each line, straight from
+	 * the Network Table.
+	 * 
+	 * @return The raw points of each line retrieved from the Network Table
+	 *         published by the "Publish LinesReport" operation.
 	 */
-	public double[][] getRawPoints(){
+	public ArrayList<ArrayList<Double>> getRawPoints() {
+		switchType(ProcessingType.Lines);
 		double[][] pointValues = new double[4][0];
-        pointValues[0] = table.getNumberArray("x1", defaultValue);
-        pointValues[1] = table.getNumberArray("x2", defaultValue);
-        pointValues[2] = table.getNumberArray("y1", defaultValue);
-        pointValues[3] = table.getNumberArray("y2", defaultValue);
-		return pointValues;
+		pointValues[0] = table.getNumberArray("x1", defaultValue);
+		pointValues[1] = table.getNumberArray("x2", defaultValue);
+		pointValues[2] = table.getNumberArray("y1", defaultValue);
+		pointValues[3] = table.getNumberArray("y2", defaultValue);
+
+		return convert(pointValues);
+
 	}
-	
+
 	/**
-	 * Gets the raw points and turns into a more manageable table, where the left column is the x value 
-	 * and the right column is the y value.
-	 * @return The points of each line retrieved from the Network Table published by the "Publish LinesReport" operation.
+	 * Gets the raw points and turns into a more manageable table, where the
+	 * left column is the x value and the right column is the y value.
+	 * 
+	 * @return The points of each line retrieved from the Network Table
+	 *         published by the "Publish LinesReport" operation.
 	 */
-	public double[][] getPoints(){
-		double[][] rawValues = getRawPoints();
-		double[][] points = new double [rawValues[0].length * 2][2];
-        
-        for (int i = 0 ; i < rawValues[0].length; i++){
-        	points[i][0] = rawValues[0][i];
-        	points[i][1] = rawValues[2][i];
-        	points[rawValues[0].length + i][0] = rawValues[1][i];
-        	points[rawValues[0].length + i][1] = rawValues[3][i];
-        }
-        
-		return points;
+	public ArrayList<ArrayList<Double>> getPoints() {
+		switchType(ProcessingType.Lines);
+		ArrayList<ArrayList<Double>> rawValues = getRawPoints();
+		ArrayList<ArrayList<Double>> re = new ArrayList<ArrayList<Double>>();
+		ArrayList<Double> x = new ArrayList<Double>();
+		ArrayList<Double> y = new ArrayList<Double>();
+
+		for (int i = 0; i < rawValues.get(0).size(); i++) {
+			x.add(rawValues.get(0).get(i));
+			y.add(rawValues.get(2).get(i));
+			x.add(rawValues.get(1).get(i));
+			y.add(rawValues.get(3).get(i));
+		}
+
+		return re;
 	}
-	
+
+	public double[][] getRectangle() {
+		return null;
+	}
+
 	/**
 	 * Gets the center of all the points based on the average x and y values.
-	 * @return The center of all the points detected in the Network Table published by the "Publish LinesReport" operation.
+	 * 
+	 * @return The center of all the points detected in the Network Table
+	 *         published by the "Publish LinesReport" operation.
 	 */
-	public double[] getAveCenter(){
-		double [][] points = getPoints();
-		
+	public double[] getAveCenter() {
+		switchType(ProcessingType.Lines);
+		ArrayList<ArrayList<Double>> points = getPoints();
+
 		int sumX = 0;
 		int sumY = 0;
-		
-		for (int i = 0; i < points.length; i++){
-			sumX += points[i][0];
-			sumY += points[i][1];
+
+		for (int i = 0; i < points.size(); i++) {
+			sumX += points.get(i).get(0);
+			sumY += points.get(i).get(1);
 		}
-		
+
 		double[] center = new double[2];
-		
-		center[0] = sumX / points.length;
-		center[1] = sumY / points.length;
-		
+
+		center[0] = sumX / points.size();
+		center[1] = sumY / points.size();
+
 		return center;
+	}
+
+	public ArrayList<Double> solidities() {
+		switchType(ProcessingType.Contours);
+		double[] solidity = table.getNumberArray("solidity", defaultValue);
+
+		return convert(solidity);
+	}
+
+	public ArrayList<ArrayList<Double>> centers() {
+		switchType(ProcessingType.Contours);
+		double[] x = table.getNumberArray("centerX", defaultValue);
+		double[] y = table.getNumberArray("centerY", defaultValue);
+		double[][] center = new double[x.length][2];
+
+		for (int i = 0; i < x.length; i++) {
+			center[i][0] = x[i];
+			center[i][1] = y[i];
+		}
+
+		return convert(center);
+	}
+
+	public ArrayList<Double> widths() {
+		switchType(ProcessingType.Contours);
+		double[] width = table.getNumberArray("width", defaultValue);
+
+		return convert(width);
+	}
+
+	public ArrayList<Double> heights() {
+		switchType(ProcessingType.Contours);
+		double[] height = table.getNumberArray("height", defaultValue);
+
+		return convert(height);
+	}
+
+	public ArrayList<Double> areas() {
+		switchType(ProcessingType.Contours);
+		double[] height = table.getNumberArray("height", defaultValue);
+
+		return convert(height);
+	}
+
+	public ArrayList<Double> xValues() {
+		switchType(ProcessingType.Blobs);
+		double[] x = table.getNumberArray("x", defaultValue);
+		
+		return convert(x);
+	}
+
+	public ArrayList<Double> yValues() {
+		switchType(ProcessingType.Blobs);
+		double[] y = table.getNumberArray("y", defaultValue);
+		
+		return convert(y);
+	}
+
+	public ArrayList<Double> sizes() {
+		switchType(ProcessingType.Blobs);
+		double[] size = table.getNumberArray("sizes", defaultValue);
+		
+		return convert(size);
+	}
+
+	public ArrayList<Double> convert(double[] values) {
+		ArrayList<Double> re = new ArrayList<Double>();
+
+		for (int i = 0; i < values.length; i++) {
+			re.add(values[i]);
+		}
+
+		return re;
+	}
+
+	public ArrayList<ArrayList<Double>> convert(double[][] values) {
+		ArrayList<ArrayList<Double>> re = new ArrayList<ArrayList<Double>>();
+
+		for (int i = 0; i < values.length; i++) {
+			re.add(new ArrayList<Double>());
+			ArrayList<Double> current = re.get(i);
+
+			for (int j = 0; j < values[0].length; j++) {
+				current.add(values[i][j]);
+			}
+		}
+
+		return re;
 	}
 }
--- src/org/oastem/frc/strong/Robot.java
+++ src/org/oastem/frc/strong/Robot.java
@@ -1,16 +1,22 @@
 package org.oastem.frc.strong;
-import org.oastem.frc.sensor.ImageProcessing;
 
+import org.oastem.frc.sensor.ImageProcessing;
+import org.oastem.frc.sensor.ImageProcessing.ProcessingType;
+
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.util.ArrayList;
+
 /**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
+ * This is a demo program showing the use of the RobotDrive class. The
+ * SampleRobot class is the base of a robot application that will automatically
+ * call your Autonomous and OperatorControl methods at the right time as
+ * controlled by the switches on the driver station or the field controls.
  *
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the SampleRobot
@@ -18,56 +24,60 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
+ * WARNING: While it may look like a good choice to use for your code if you're
+ * inexperienced, don't. Unless you know what you are doing, complex code will
+ * be much more difficult under this system. Use IterativeRobot or Command-Based
+ * instead if you're new.
  */
 public class Robot extends SampleRobot {
-    Joystick stickLeft;
-    Joystick stickRight;
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    SendableChooser chooser;
-    SmartDashboard dash;
-    
-    ImageProcessing process;
-    
-    
-    public Robot() {
-    	stickLeft = new Joystick(0);
-    	stickLeft = new Joystick(1);
-        process = new ImageProcessing("GRIP/myLinesReport");
-    }
-    
-    public void robotInit() {
-    	chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        
-        dash = new SmartDashboard();
-        SmartDashboard.putData("Auto modes", chooser);
-    }
+	Joystick stickLeft;
+	Joystick stickRight;
+	final String defaultAuto = "Default";
+	final String customAuto = "My Auto";
+	SendableChooser chooser;
+	SmartDashboard dash;
 
-    /**
-     * Runs the motors with arcade steering.
-     */
-    public void operatorControl() {
-        while (isOperatorControl() && isEnabled()) {
-        	//double[] angles = process.getAngles();
-        	//double[] lengths = process.getLengths();
-        	double[][] points = process.getPoints();
-        	
-            for (int i = 0; i < points.length; i++){
-            	dash.putString("Point " + (i + 1), points[i][0] + ", " + points[i][1]);
-            }
-            
-        }
-    }
+	ImageProcessing process;
 
-    /**
-     * Runs during test mode
-     */
-    public void test() {
-    }
-    
+	public void robotInit() {
+		chooser = new SendableChooser();
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+
+		dash = new SmartDashboard();
+		dash.putData("Auto modes", chooser);
+
+		stickLeft = new Joystick(0);
+		stickLeft = new Joystick(1);
+		process = new ImageProcessing("GRIP/myContoursReport", "GRIP/myLinesReport", "GRIP/myBlobsReport",
+				ProcessingType.Lines);
+
+	}
+
+	public void operatorControl() {
+		dash.putString("wtf", "why");
+		while (isOperatorControl() && isEnabled()) {
+			//double[][] points = process.getPoints();
+			//dash.putNumber("length", points.length);
+			
+			ArrayList<Double> swag = process.getAngles();
+			
+			for (int i = 0; i < swag.size(); i++) {
+				dash.putNumber("Angle " + (i + 1),  swag.get(i));
+				//dash.putString("Point " + (i + 1), points[i][0] + ", " + points[i][1]);
+			
+			}
+
+			// 182.2 inches
+			// 15 ft 2 in
+
+		}
+	}
+
+	/**
+	 * Runs during test mode
+	 */
+	public void test() {
+	}
+
 }
