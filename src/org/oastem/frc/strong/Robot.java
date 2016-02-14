348aa430b1af0875f7cdd28ef03c6728141b7247
Joie Le <joie188@gmail.com>
2016-02-14 14:02:46 -0800

    Add state machine to move arm

--- src/org/oastem/frc/strong/Robot.java
+++ src/org/oastem/frc/strong/Robot.java
@@ -1,15 +1,16 @@
 package org.oastem.frc.strong;
+import org.oastem.frc.LogitechGamingPad;
 import org.oastem.frc.control.DriveSystem;
 import org.oastem.frc.control.TalonDriveSystem;
+import org.oastem.frc.sensor.FRCGyroAccelerometer;
+import org.oastem.frc.sensor.QuadratureEncoder;
 
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
-import org.oastem.frc.sensor.FRCGyroAccelerometer;
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -39,11 +40,27 @@ public class Robot extends SampleRobot {
 	private final int DRIVE_WHEEL_DIAM = 6;
 	private final int LEFT_CAN_DRIVE = 0;
 	private final int RIGHT_CAN_DRIVE = 1;
+	
+	//Arm States
+	private final int TOP_STATE = 0; 
+	private final int MIDDLE_TOP_STATE = 1;
+	private final int MIDDLE_BOTTOM_STATE = 3;
+	private final int BOTTOM_STATE = 4;
+	private final int MANUAL_STATE = 5;
+	private final int E_STOP_STATE = 6;
+		private final int MAX_ARM_VALUE = 180; //for now
+		private final int MID_TOP_ARM_VALUE = 135;
+		private final int MID_BOTTOM_ARM_VALUE = 45;
+		private final int MIN_ARM_VALUE = 0; //for now
+	
 	private static double joyScale = 1.0;
     DriveSystem myRobot = DriveSystem.getInstance();
     TalonDriveSystem talonDrive = TalonDriveSystem.getInstance();
-    Joystick stickLeft;
-    Joystick stickRight;
+    //Joystick stickLeft;
+    //Joystick stickRight;
+    Talon armMotor; 
+    LogitechGamingPad pad;
+    QuadratureEncoder armPositionEncoder;
     final String defaultAuto = "Default";
     final String customAuto = "My Auto";
     SendableChooser chooser;
@@ -51,16 +68,15 @@ public class Robot extends SampleRobot {
     FRCGyroAccelerometer gyro;
     SmartDashboard dash;
     BuiltInAccelerometer accel;
-    
-   
- 
-      
+       
     public Robot() {
-    	myRobot.initializeDrive(FRONT_LEFT_DRIVE, BACK_LEFT_DRIVE, FRONT_RIGHT_DRIVE, BACK_RIGHT_DRIVE); //WE ARE SMART
+    	myRobot.initializeDrive(FRONT_LEFT_DRIVE, BACK_LEFT_DRIVE, FRONT_RIGHT_DRIVE, BACK_RIGHT_DRIVE); 
     	//talonDrive.initializeTalonDrive(LEFT_CAN_DRIVE, RIGHT_CAN_DRIVE, DRIVE_ENC_CODE_PER_REV, DRIVE_WHEEL_DIAM);
-        stickLeft = new Joystick(0);
-        stickRight = new Joystick(1);
-        
+        //stickLeft = new Joystick(0);
+        //stickRight = new Joystick(1);
+    	pad = new LogitechGamingPad(0);
+    	armPositionEncoder = new QuadratureEncoder(0,1,2); // i dont know what i did FIX LATER i actually knew what i was doing
+    	armMotor = new Talon(0);
         test = new CANTalon(0);
         test.changeControlMode(TalonControlMode.Speed);
         test.reverseSensor(true);
@@ -88,14 +104,15 @@ public class Robot extends SampleRobot {
      */
     public void operatorControl() {
     	gyro.resetGyro();
-    	//gyro.freeAccel();
+    	int stateOfArm = BOTTOM_STATE; 
+        boolean isManualState = false;
+    	int goalValue = 0;
         while (isOperatorControl() && isEnabled()) {
             //myRobot.arcadeDrive(stickLeft.getY(), stickLeft.getX()); // drive with arcade style (use right stick)
             //myRobot.tankDrive(stickLeft.getY(), stickRight.getY());
         	//doArcadeDrive();
         	//talonDrive.speedTankDrive(stickLeft.getY(), stickRight.getY(), false);
         	test.set(60);
-        	
         	dash.putNumber("Gyro Value:", gyro.getGyroAngle());
         	dash.putNumber("Accelerometer X Value: ", gyro.getAccelX());
         	dash.putNumber("Accelerometer Y Value: ", gyro.getAccelY());
@@ -103,6 +120,96 @@ public class Robot extends SampleRobot {
         	dash.putNumber("Built-In Accelerometer X Value: ", accel.getX());
         	dash.putNumber("Built-In Accelerometer Y Value: ", accel.getY());
         	dash.putNumber("Built-In Accelerometer Z Value: ", accel.getZ()-1);
+        	
+        	int encoderValue = armPositionEncoder.get();
+        	//toggle button is the b button
+        	if ( pad.getBButton() )
+        		isManualState = !isManualState;
+        	if (isManualState)
+        		stateOfArm = MANUAL_STATE;
+        	switch (stateOfArm)
+        	{        		
+        		case TOP_STATE :
+        			goalValue = MAX_ARM_VALUE;
+        			if ( encoderValue > goalValue)
+        				//go down
+        				armMotor.set(-.5);
+	        		if (pad.getAButton())
+	        			stateOfArm = MIDDLE_TOP_STATE;
+	        		else if (pad.getBButton())
+	        		{
+	        			isManualState = !isManualState;
+	        			stateOfArm = MANUAL_STATE;
+	        		}
+        			dash.putString("State: ", "top state");
+        			break;
+        		case MIDDLE_TOP_STATE :
+        			goalValue = MID_TOP_ARM_VALUE;
+        			if ( encoderValue > goalValue)
+        				//go down
+        				armMotor.set(-.5);
+        			else if (encoderValue < goalValue)
+	        			//go up
+        				armMotor.set(.5);
+	        		if (pad.getYButton())
+	        			stateOfArm = TOP_STATE;
+	        		else if (pad.getAButton())
+	        			stateOfArm = MIDDLE_BOTTOM_STATE;
+	        		else if (pad.getBButton())
+	        		{
+	        			isManualState = !isManualState;
+	        			stateOfArm = MANUAL_STATE;
+	        		}
+        			dash.putString("State: ", "mid-top state");
+        			break;
+        		case MIDDLE_BOTTOM_STATE :
+        			goalValue = MID_BOTTOM_ARM_VALUE;
+        			if ( encoderValue > goalValue)
+        				armMotor.set(-.5);
+        			else if (encoderValue < goalValue)
+        				armMotor.set(.5);
+	        		if (pad.getYButton())
+	        			stateOfArm = MIDDLE_TOP_STATE;
+	        		else if (pad.getAButton())
+	        			stateOfArm = BOTTOM_STATE;
+	        		else if (pad.getBButton())
+	        		{
+	        			isManualState = !isManualState;
+	        			stateOfArm = MANUAL_STATE;
+	        		}
+        			dash.putString("State: ", "mid-bottom");
+        			break;
+        		case BOTTOM_STATE :
+        			goalValue = MIN_ARM_VALUE;
+        			if ( encoderValue > goalValue)
+        				armMotor.set(-.5);
+        			else if (encoderValue < goalValue)
+        				armMotor.set(.5);
+	        		if (pad.getYButton())
+	        			stateOfArm = MIDDLE_BOTTOM_STATE;
+	        		else if (pad.getBButton())
+	        		{
+	        			isManualState = !isManualState;
+	        			stateOfArm = MANUAL_STATE;
+	        		}
+        			dash.putString("State: ", "bottom");
+        			break;
+        		case MANUAL_STATE :
+        			if(pad.getYButton() && encoderValue < MAX_ARM_VALUE) 
+        				armMotor.set(.75);
+        			else if (pad.getAButton() && encoderValue > MIN_ARM_VALUE ) 
+        				armMotor.set(-.75);
+        			if (pad.getBButton())
+	        		{
+	        			isManualState = !isManualState;
+	        			//what would stateOfArm be?
+	        		}
+        			break;
+        		case E_STOP_STATE :
+        			armMotor.set(0);
+        			//stop everything....but what is everything??
+        			break;
+        	}
         }
     }
 
@@ -112,7 +219,7 @@ public class Robot extends SampleRobot {
     public void test() {
     }
     
-    private void doArcadeDrive() {
+   /* private void doArcadeDrive() {
 		double leftMove = 0.0;
 		double rightMove = 0.0;
 		double zone = 0.04;
@@ -151,6 +258,6 @@ public class Robot extends SampleRobot {
 		if (val < -1.0) {
 			return -1.0;
 		}
-		return val;
-	}
+		return val; 
+	}*/
 }
